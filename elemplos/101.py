1a319da0c24fa6f47a6f4d4d8611325b1f9c103d
pomaresalejandro <pomares.alejandro@gmail.com>
2024-01-04 19:11:46 +0000

    Pending changes exported from your codespace

---
+++ elemplos/101.py
@@ -0,0 +1,6 @@
+# Suma dos numeros asdfm sdafs asdlkfjojisa dfuiasdufouasdoiuf oafsduodiod fuiosduifo uods ufo dufsa odfu odsfu oadsfuj
+
+x = int(input('x='))
+y = int(input('y='))
+y = print('x+y = ',x+y)
+
